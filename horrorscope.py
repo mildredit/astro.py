af4e1b4686f5f29dba4719a12d74e7de05a32930
mildredit <milyituarte@gmail.com>
2023-03-23 15:58:38 -0700

    class added

--- horrorscope.py
+++ horrorscope.py
@@ -1,42 +1,84 @@
 import requests
 #  Signs (Aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces)
 
-
-def get_zodiac_sign():
-    zodiac_sign = input("Enter your zodiac sign: ")
-    return zodiac_sign
+# sign = ("Aries", "taurus")
 
 
-def get_day():
-    day = input("Enter the horoscope day (today, tomorrow, or yesterday): ")
-    return day
+class Astrogame:
+    url = 'https://aztro.sameerkumar.website/'
+    valid_signs = ("aries", "taurus", "gemini", "cancer", "leo", "virgo",
+                   "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces")
+    sign = ""
+    day = ""
+
+    def get_zodiac_sign(self):
+        zodiac_sign = input("Enter your zodiac sign: ")
+        self.sign = zodiac_sign
+        return zodiac_sign
+
+    def get_day(self) -> str:
+        day = input("Enter the horoscope day (today, tomorrow, or yesterday): ")
+        self.day = day
+        return day
+
+    def your_sign(self, sign):
+        if sign.lower() in self.valid_signs:
+            print("\nThe reading for", sign, "is: ")
+        else:
+            print("No data available")
+        return sign
+
+    def is_valid_sign(self, sign) -> bool:
+        """returns true if sign is valid, false if not"""
+        return sign in self.valid_signs
+
+    def request_astro_info(self):
+        """Given a day and sign, make request to get data from astro api"""
+        params = (('sign', self.sign), ('day', self.day))
+        response = requests.post(self.url, params=params)
+        return response.json()
+
+    def print_reading(self, response_json):
+        """print reading from the api call"""
+        print("\nHoroscope for", response_json.get('current_date'), '\n')
+        print(response_json.get('description'), '\n')
+        print('Compatibility:', response_json.get('compatibility'))
+        print('Mood:', response_json.get('mood'))
+        print('Color:', response_json.get('color'))
+        print('Lucky Number:', response_json.get('lucky_number'))
+        print('Lucky Time:', response_json.get('lucky_time'), "\n")
 
 
-def your_sign():
-    sign = get_zodiac_sign()
-    if sign == "leo":
-        print("X")
-    elif sign == "Taurus":
-        print("Z")
-    else:
-        print("E")
-    return your_sign
+astro = Astrogame()
+astro.get_day()
+astro.get_zodiac_sign()
+astro.your_sign(astro.sign)
+if astro.is_valid_sign(astro.sign):
+    # astro.your_sign(astro.sign)
+    response = astro.request_astro_info()
+    astro.print_reading(response)
 
 
-# parameters for the HTTP request (Aztro)
-params = (('sign', get_zodiac_sign()), ('day', get_day()))
+# sign = astro.get_zodiac_sign()
+# day = astro.get_day()
+# astro.your_sign(sign)
 
-response = requests.post('https://aztro.sameerkumar.website/', params=params)
-# print(response.status_code)
-# print(response.text)
-# print(params)
-# print(response.json())
-json = response.json()
+# if astro.is_valid_sign(sign):
+    # parameters for the HTTP request (Aztro)
+    # params = (('sign', sign), ('day', day))
 
-print("\nHoroscope for", json.get('current_date'), '\n')
-print(json.get('description'), '\n')
-print('Compatibility:', json.get('compatibility'))
-print('Mood:', json.get('mood'))
-print('Color:', json.get('color'))
-print('Lucky Number:', json.get('lucky_number'))
-print('Lucky Time:', json.get('lucky_time'), "\n")
+    # response = requests.post(
+    #     'https://aztro.sameerkumar.website/', params=params)
+    # print(response.status_code)
+    # print(response.text)
+    # print(params)
+    # print(response.json())
+    # json = response.json()
+
+    # print("\nHoroscope for", json.get('current_date'), '\n')
+    # print(json.get('description'), '\n')
+    # print('Compatibility:', json.get('compatibility'))
+    # print('Mood:', json.get('mood'))
+    # print('Color:', json.get('color'))
+    # print('Lucky Number:', json.get('lucky_number'))
+    # print('Lucky Time:', json.get('lucky_time'), "\n")
